792f465882dcc7698048bb46bac26a9fb05c1a42
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-12-13 11:57:52 -0300

    style: added fonts, assets and global styles

---
+++ src/assets/icons/index.tsx
@@ -0,0 +1,10 @@
+import {
+    RiCloseFill as Close,
+    RiMoneyDollarCircleLine as Money,
+} from 'react-icons/ri';
+import {
+    BsArrowDownCircle as Expense,
+    BsArrowUpCircle as Income,
+} from 'react-icons/bs';
+
+export { Expense, Income, Money, Close };
