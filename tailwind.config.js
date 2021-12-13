c97e09a7232f6e4cea51edf2d24d1559b4caf519
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-12-13 11:53:01 -0300

    style: added assets and styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
     content: ['./src/**/*.{ts,tsx,js,jsx}', './public/index.html'],
     darkMode: 'class', // or 'media' or 'false'
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                background: '#F0F2F5',
+            },
+        },
     },
     variants: {
         extend: {},
