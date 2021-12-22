d13ab43d6893ff85b63ecb896f80ec1e7f6a82f1
Guilhermy <55157846+devguilhermy@users.noreply.github.com>
2021-12-22 20:24:35 -0300

    test: link dinâmico conforme ambiente

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,8 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://localhost:3000/api',
+    baseURL:
+        process.env.NODE_ENV === 'production'
+            ? 'https://moneyful-live.vercel.app/api'
+            : 'https://localhost:3000/api',
 });
